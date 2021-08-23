6aea0709f44b8886ab308bb722e11e5d139540ca
Ryan <rrpizani@gmail.com>
2021-08-23 10:16:42 -0300

    terminando funcoes comuns

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
-let a
-let a
-var idade = 18
-console.log ("Oi, " + nome)
-if (idade >= 18){
-     let nome = "João"
-     console.log ("Parabéns " + nome + ". Você já pode dirigir")
+const triplo = function (n = 5){
+  return n * 3
 }
+console.log (triplo(8))
+console.log(triplo())
 
- console.log ("Até mais, " + nome)
+// function eAgora(f1, f2) {
+//   return f1() * f2()
+// }
 
+// console.log(eAgora (function () { return 2 + 3}, function(){2 * 3}))
 
-// var linguagem = "Javascript"
-// console.log ("Aprendendo " + linguagem)
-// //variavel pode ser redeclarada
-// var linguagem = "Java"
-// console.log ('Aprendendo ' + linguagem)
+// // const dobro = function (n) {
+// // return n * 2
 
-// //declarando variáveis
-// let a = 2
-// let d
-// let b = "abcd"
-// a = 3
+// // }
 
-// //declarando constantes
-// const nome = "José"
-// const idade = 27
-// const endereco = 'Rua K, 12'
-// //nome = "Maria"
+// // const res = dobro (5)
+// // console.log(res)
+
+// // function soma (a,b){
+// //      if(a===b){
+// //           return a + b
+// //      }
+
+// // }
+
+// // const resultado1 = soma(3, 3)
+// // const resultado2 =
+// // console.log ('Resultado 1: ${resultado1}. Resultado 2: ${resultado2}')
+
+// //function hello(){
+// //console.log('Hello')
+// //}
+
+// //hello()
+
+// //function hello(nome){
+// // console.log ('Hello, ${nome}') //console.log("Hello", + nome)
+// //}
+
+// //hello('José')
+
+// //let a
+// //let a
+// //var idade = 18
+// //console.log ("Oi, " + nome)
+// //if (idade >= 18){
+// //let nome = "João"
+// //console.log ("Parabéns " + nome + ". Você já pode dirigir")
+// //}
+
+// //console.log ("Até mais, " + nome)
+
+// // var linguagem = "Javascript"
+// // console.log ("Aprendendo " + linguagem)
+// // //variavel pode ser redeclarada
+// // var linguagem = "Java"
+// // console.log ('Aprendendo ' + linguagem)
+
+// // //declarando variáveis
+// // let a = 2
+// // let d
+// // let b = "abcd"
+// // a = 3
+
+// // //declarando constantes
+// // const nome = "José"
+// // const idade = 27
+// // const endereco = 'Rua K, 12'
+// // //nome = "Maria"
