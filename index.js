a1e6c5c549368782bd1104750b2941c2f40872fb
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-23 09:31:37 -0300

    Encerrada a parte de variaveis da apostila 03

---
+++ index.js
@@ -0,0 +1,29 @@
+let a
+let a
+var idade = 18
+console.log ("Oi, " + nome)
+if (idade >= 18){
+     let nome = "João"
+     console.log ("Parabéns " + nome + ". Você já pode dirigir")
+}
+
+ console.log ("Até mais, " + nome)
+
+
+// var linguagem = "Javascript"
+// console.log ("Aprendendo " + linguagem)
+// //variavel pode ser redeclarada
+// var linguagem = "Java"
+// console.log ('Aprendendo ' + linguagem)
+
+// //declarando variáveis
+// let a = 2
+// let d
+// let b = "abcd"
+// a = 3
+
+// //declarando constantes
+// const nome = "José"
+// const idade = 27
+// const endereco = 'Rua K, 12'
+// //nome = "Maria"
